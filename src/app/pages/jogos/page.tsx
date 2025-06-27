4ff321cdb487b56a0b0c54017babfd5a9a0fdd9d
Matheus Nogueira Moreira <90008448+MatheusNogMoreira@users.noreply.github.com>
2025-06-27 18:04:14 -0300

    Atualização em Jogos - Mídias com todas as Info sendo exibidas

--- src/app/pages/jogos/page.tsx
+++ src/app/pages/jogos/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Midia_card from "@/app/components/midia_card/Midia_card";
 import "./Games.css";
-import Link from "next/link";
 
 interface Midia {
   id: number;
   nome_midia: string;
   tipo_midia: string;
   endereco_imagem: string;
+  descricao: string;
 }
 
 export default function Games() {
@@ -18,23 +19,22 @@ export default function Games() {
     fetch("http://localhost:3000/api/midias")
       .then((res) => res.json())
       .then((data: Midia[]) => {
-        setGames(data.filter((item) => item.tipo_midia.toLowerCase() === "jogo"));
+        const jogos = data.filter(
+          (item) => item.tipo_midia.toLowerCase() === "jogo"
+        );
+        setGames(jogos);
       })
       .catch((error) => console.error("Erro ao buscar jogos:", error));
   }, []);
 
   return (
     <main className="pageGames">
-      <h1>Jogos Perdidos</h1>
+      <h1 className="titulo-centralizado">Jogos Perdidos</h1>
       <div className="grid">
         {games.map((game) => (
-          <Link key={game.id} href={`/midia/${game.id}`} className="card">
-            <img
-              src={`http://localhost:3000${game.endereco_imagem}`}
-              alt={game.nome_midia}
-            />
-            <h2>{game.nome_midia}</h2>
-          </Link>
+          <Midia_card key={game.id} id={game.id}>
+            {game.nome_midia}
+          </Midia_card>
         ))}
       </div>
     </main>
